5e61c94048dd1f970b069e8d40767e5f93827df7
Kawamura Shintaro <kawasin73@gmail.com>
2018-12-31 01:47:52 +0900

    Add comment about structure

--- 9cc.c
+++ 9cc.c
@@ -89,6 +89,19 @@ Node *new_node_num(int val) {
     return node;
 }
 
+// Parser Structure
+//
+// expr: mul
+// expr: mul "+" expr
+// expr: mul "-" expr
+// mul: term
+// mul: term "*" mul
+// mul: term "/" mul
+// term: number
+// term: "(" expr ")"
+// number: digit | digit number
+// digit: "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+
 Node *mul();
 Node *term();
 
