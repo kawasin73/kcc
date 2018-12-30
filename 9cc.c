34e9b875cc850db5c5711e99a03754fcdcfc680c
Kawamura Shintaro <kawasin73@gmail.com>
2018-12-30 17:44:48 +0900

    Add sub and add

--- 9cc.c
+++ 9cc.c
@@ -7,10 +7,30 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global _main\n");
     printf("_main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+    while(*p) {
+        if (*p == '+') {
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期せぬ文字です: '%c'\n", *p);
+        return 1;
+    }
+    
     printf("  ret\n");
     return 0;
 }
